b76e670e9ad38bd4ec0c3add6c7990dfa1a471d4
RafatSh-hash <rafatsh800@gmail.com>
2023-02-24 20:01:46 +0600

    project initialized

---
+++ src/Main/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Foot from '../Components/Shared/Foot/Foot';
+import Navigation from '../Components/Shared/Navigation';
+
+const Main = () => {
+    return (
+        <div>
+            <Navigation></Navigation>
+            <Outlet></Outlet>
+            <Foot></Foot>
+        </div>
+    );
+};
+
+export default Main;
