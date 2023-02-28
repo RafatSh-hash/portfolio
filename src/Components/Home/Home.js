367b9879c3ab38f3cd29d1766506c63590d6f6fe
RafatSh-hash <rafatsh800@gmail.com>
2023-02-28 18:17:58 +0600

    projejects adding

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -13,9 +13,6 @@ const Home = () => {
                 <p className='font-bold sm:text-center md:text-center lg:text-left text-white'>MERN Stack developer</p>
                
                     <button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Check Out My Resume</button>
-                   
-
-               
                 <p className='mt-20 text-red-400'>"Being a programmer is not a task of one or several days, but a journey for lifetime. So Pracitce , Practice & Practice..."</p>
                 <p className='text-right mx-20 font-bold'>- Jhankar Mahbub</p>
             </div>
