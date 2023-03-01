1f007517bb14c7517318a5d5772547ac72dbb07e
RafatSh-hash <rafatsh800@gmail.com>
2023-03-01 21:43:22 +0600

    some responsive issue fixed

---
+++ src/Components/Contact/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div className='w-[90%] mx-auto'>
+             <h1 style={{fontFamily: 'Fira Code'}} className='text-5xl sm:text-center md:text-left text-left my-10 text-white font-bold'>Contact info</h1>
+             
+        </div>
+    )
+}
+
+export default Contact;
