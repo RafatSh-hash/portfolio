a42178bf00b15cfe48259b50be1d9b12255c2d23
RafatSh-hash <rafatsh800@gmail.com>
2023-03-04 19:33:43 +0600

    button position fixed

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -38,15 +38,17 @@ const Home = () => {
               deleteSpeed={100}
             ></Typewriter>
           </p>
+          <div className="sm:w-1/2 sm:mx-auto">
+            <button className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 mt-5 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 w-full ">
+              <a
+                href="https://drive.google.com/file/d/1Cqymtz4t222L9uhwyz1Jzw17HzJ43okg/view?usp=sharing"
+                target="_blank"
+              >
+                Check Out My Resume
+              </a>
+            </button>
+          </div>
 
-          <button className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 mt-5 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 sm:w-1/2 sm:mx-auto">
-            <a
-              href="https://drive.google.com/file/d/1Cqymtz4t222L9uhwyz1Jzw17HzJ43okg/view?usp=sharing"
-              target="_blank"
-            >
-              Check Out My Resume
-            </a>
-          </button>
           <p className="mt-14 text-xl text-gray-400 text-justify lg:w-[80%] min-[320px]:w-full sm:w-full">
             "Being a programmer is not a task of one or several days, but a
             journey for lifetime. So Pracitce , Practice & Practice..."
